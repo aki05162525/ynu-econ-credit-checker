0f6c39635108e8965216864fd94ab079bd922056
aki05162525 <akihiro05162525@gmail.com>
2025-08-20 01:02:19 +0900

    テスト

--- frontend/src/test-file.ts
+++ frontend/src/test-file.ts
@@ -1,6 +1,6 @@
 // テスト用のファイル（意図的にフォーマットを崩している）
-const   testFunction = (  x : number,y:number ) =>   {
-      return x+y+1;
-}
+const testFunction = (x: number, y: number) => {
+	return x + y + 1;
+};
 
 export default testFunction;
