fccef10a9b366739757e47bb1b2b0c3fb1318227
aki05162525 <akihiro05162525@gmail.com>
2025-08-21 22:00:17 +0900

    Headerコンポーネントにタイトルプロパティを追加し、デフォルトタイトルを設定

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
-export const Header = () => {
+export type HeaderProps = {
+	title?: string;
+};
+
+export const Header = ({ title = "YNU-econ 単位チェッカー" }: HeaderProps) => {
 	return (
 		<header className="flex items-center justify-center bg-brand p-4 shadow-sm">
-			<h1 className="font-semibold text-xl text-white">
-				YNU-econ 単位チェッカー
-			</h1>
+			<h1 className="font-semibold text-xl text-white">{title}</h1>
 		</header>
 	);
 };
