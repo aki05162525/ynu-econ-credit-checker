8d838cbe4c7ca81be446a33ee862eff833cf8d19
aki05162525 <akihiro05162525@gmail.com>
2025-09-26 17:40:01 +0900

    testの設定

--- frontend/src/components/layout/Header/Header.test.tsx
+++ frontend/src/components/layout/Header/Header.test.tsx
@@ -1,8 +1,9 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import { Header } from "../Header";
 
-describe("Header コンポーネント", () => {
+//todo スナップショットテストはこのコンポーネントでは不要だが練習のため取っておく
+describe("Header コンポーネントスナップショットテスト", () => {
 	test("デフォルトのタイトルで表示される", () => {
 		const { container } = render(<Header />);
 		expect(container).toMatchSnapshot();
@@ -11,5 +12,31 @@ describe("Header コンポーネント", () => {
 	test("カスタムタイトルで表示される", () => {
 		const { container } = render(<Header title="単位チェックアプリ" />);
 		expect(container).toMatchSnapshot();
+	});
+});
+
+describe("Header コンポーネント", () => {
+	test("カスタムタイトルを表示する", () => {
+		const title = "単位チェックアプリ";
+		render(<Header title={title} />);
+		expect(
+			screen.getByRole("heading", { level: 1, name: title }),
+		).toBeInTheDocument();
+	});
+
+	test("デフォルトタイトルを表示する", () => {
+		render(<Header />);
+		expect(
+			screen.getByRole("heading", {
+				level: 1,
+				name: "YNU-econ 単位チェッカー",
+			}),
+		).toBeInTheDocument();
+	});
+
+	test("タイトルのリンク先は / である", () => {
+		render(<Header />);
+		const link = screen.getByRole("link");
+		expect(link).toHaveAttribute("href", "/");
 	});
 });
--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -5,6 +5,8 @@ export default defineConfig({
 	plugins: [react()],
 	test: {
 		environment: "jsdom",
+		setupFiles: "./vitest.setup.ts",
+		globals: true,
 		coverage: {
 			provider: "istanbul", // or 'v8'
 			reporter: ["text", "json", "html"],
