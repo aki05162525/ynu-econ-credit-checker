d6d7dcd282a7c2dca9e66349d4028a4fcfe60197
TAKAGI AKIHIRO <156570014+aki05162525@users.noreply.github.com>
2025-09-26 17:48:43 +0900

    gh-66 setup vitest (#67)
    * testの導入
    
    * 不要フォルダの削除
    
    * package.jsonにvitestと関連する依存関係を追加し、vitest.config.tsを新規作成
    
    * coverageの導入
    
    * testの設定
    
    * CIの設定を更新し、テストとカバレッジ生成のステップを追加
    
    * TODOコメントの表記を修正し、vitestの設定を整理

---
+++ frontend/src/components/layout/Header/Header.test.tsx
@@ -0,0 +1,42 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import { Header } from "../Header";
+
+//TODO スナップショットテストはこのコンポーネントでは不要だが練習のため取っておく
+describe("Header コンポーネントスナップショットテスト", () => {
+	test("デフォルトのタイトルで表示される", () => {
+		const { container } = render(<Header />);
+		expect(container).toMatchSnapshot();
+	});
+
+	test("カスタムタイトルで表示される", () => {
+		const { container } = render(<Header title="単位チェックアプリ" />);
+		expect(container).toMatchSnapshot();
+	});
+});
+
+describe("Header コンポーネント", () => {
+	test("カスタムタイトルを表示する", () => {
+		const title = "単位チェックアプリ";
+		render(<Header title={title} />);
+		expect(
+			screen.getByRole("heading", { level: 1, name: title }),
+		).toBeInTheDocument();
+	});
+
+	test("デフォルトタイトルを表示する", () => {
+		render(<Header />);
+		expect(
+			screen.getByRole("heading", {
+				level: 1,
+				name: "YNU-econ 単位チェッカー",
+			}),
+		).toBeInTheDocument();
+	});
+
+	test("タイトルのリンク先は / である", () => {
+		render(<Header />);
+		const link = screen.getByRole("link");
+		expect(link).toHaveAttribute("href", "/");
+	});
+});
---
+++ frontend/vitest.config.ts
@@ -0,0 +1,15 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+	plugins: [react()],
+	test: {
+		environment: "jsdom",
+		setupFiles: "./vitest.setup.ts",
+		globals: true,
+		coverage: {
+			provider: "istanbul",
+			reporter: ["text", "json", "html"],
+		},
+	},
+});
